678320259b4be031c0f6b9a54b63d673d8676e3c
mukesh kumar <mukeshkumar130803@gmail.com>
2022-09-06 21:29:17 +0530

    function and return statment

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 let global=10;
 function num(){
-    let local=5;
+    var local=5;
 }
 //! console.log(global);
 //! console.log(local);
 
+var outWer="T-shirt";
+function myOutFiit(){
+    var outWer="sweater";
+    return outWer;
+}
+//! console.log(myOutFiit());
+//! console.log(outWer);
+
+function addFive(num){
+    return num+5;
+}
+//! console.log(addFive(10));
+
+function divTwo(num){
+    return num/2;
+}
+//! console.log(divTwo(20));
+
+function sum(num1,num2){
+    return num1+num2;
+}
+var result=sum(10,15);
+//! console.log(result);
